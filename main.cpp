46480b19477111c464acd462259007fb8d519709
Igor <68248740+richard96292@users.noreply.github.com>
2022-09-27 19:25:40 +0300

    Handle command line arguments

--- main.cpp
+++ main.cpp
@@ -1,12 +1,59 @@
+#include <getopt.h>
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <sstream>
 #include "hash.hpp"
-
 using namespace std;
 
-int main() {
+string outputHelp() {
+  stringstream res;
+  res << "pass -f and a filename to read from a file\n"
+      << "pass -s to read from stdin\n";
+  return res.str();
+}
+
+inline string readFromFile(const string& path) {
+  ostringstream buf;
+  ifstream file(path);
+  file.close();
+  buf << file.rdbuf();
+  return buf.str();
+}
+
+int main(int argc, char* argv[]) {
+  int c, index;
   string message;
-  getline(std::cin, message);
   Hash h;
-  cout << h.HashingFunction(message) << endl;
+
+  if (argc == 1) {
+    cout << outputHelp();
+    return 42;
+  }
+
+  while ((c = getopt(argc, argv, "f:sh")) != -1) {
+    switch (c) {
+      case 'f':
+        cerr << "reading from file " << optarg << endl;
+        cout << h.HashingFunction(readFromFile(optarg)) << endl;
+        break;
+      case 's':
+        cerr << "working with a string\n";
+        getline(std::cin, message);
+        cout << h.HashingFunction(message) << endl;
+        break;
+      case 'h':
+        cout << outputHelp();
+        break;
+      case '?':
+        return 42;
+      default:
+        abort();
+    }
+  }
+
+  for (index = optind; index < argc; index++)
+    printf("Non-option argument %s\n", argv[index]);
+
   return 0;
 }
