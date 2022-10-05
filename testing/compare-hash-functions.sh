7b61d4ac30d0acbf5d2f8d728e3ad1a18c9ba58d
Igor <68248740+richard96292@users.noreply.github.com>
2022-10-05 21:49:22 +0300

    Test hash functions from other students

---
+++ testing/compare-hash-functions.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+echo "Igor Repkin CHM"
+cd ..
+make
+
+echo "long"
+perf stat -r 10 -B ./bin -f testing/long.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+echo "very long"
+perf stat -r 10 -B ./bin -f testing/very-long.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+echo "even longer"
+perf stat -r 10 -B ./bin -f testing/even-longer.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+
+echo "Maksim Barbasov MYSHA"
+cd hash-generator
+g++ src/*.cpp lib/hash-library/{sha256,md5,sha1,keccak}.cpp -o 'bin'
+
+echo "long"
+perf stat -r 10 -B ./bin ../testing/long.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+echo "very long"
+perf stat -r 10 -B ./bin ../testing/very-long.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+echo "even longer"
+perf stat -r 10 -B ./bin ../testing/even-longer.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+
+echo "Markas Vielavicius BCE"
+cd ../BlockChainsExercises
+mkdir -pv build
+cd build
+cmake -B . -S ..
+make
+
+echo "long"
+perf stat -r 10 -B ./HashAlgorithm -f ../../testing/long.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+echo "very long"
+perf stat -r 10 -B ./HashAlgorithm -f ../../testing/very-long.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+echo "even longer"
+perf stat -r 10 -B ./HashAlgorithm -f ../../testing/even-longer.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+
+echo "Deividas Roščenkovas BCP"
+cd ../../Blockchain-Pratybos
+make
+
+echo "long"
+perf stat -r 10 -B ./prog ../testing/long.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+echo "very long"
+perf stat -r 10 -B ./prog ../testing/very-long.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+echo "even longer"
+perf stat -r 10 -B ./prog ../testing/even-longer.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+
+echo "Eimantas Katinas HG"
+cd ../hash-generatorius
+make
+
+echo "long"
+perf stat -r 10 -B ./hash -f ../testing/long.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+echo "very long"
+perf stat -r 10 -B ./hash -f ../testing/very-long.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+echo "even longer"
+perf stat -r 10 -B ./hash -f ../testing/even-longer.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+
+echo "Liudvikas Radvilavičius JAVAHASH"
+cd ../2K-3S-BGT
+javac src/lt/vu/mif/stud/*.java
+
+echo "long"
+perf stat -r 10 -B java lt.vu.mif.stud.Main -f ../../testing/long.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+# echo "very long"
+# perf stat -r 10 -B java lt.vu.mif.stud.Main -f ../../testing/long.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
+# echo "even longer"
+# perf stat -r 10 -B java lt.vu.mif.stud.Main -f ../../testing/long.txt 2>&1 >/dev/null | tail -n 2 | sed 's/ \+//' | sed 's/ /,/'
--- testing/run-all.sh
+++ testing/run-all.sh
@@ -18,9 +18,7 @@ cd .. && make && cd testing
 # run tests
 source test-2.sh
 source final-tests.sh
-
-[ -z ${var+x} ] && ./additional.sh
+source additional.sh
 
 # create a graph from -b output
 # cat data.txt | awk '{ print $2 " " $5}' | gnuplot -p -e 'set key bottom right; set format y "%.5f"; set xrange [0:520]; set terminal png size 400,300; set output "graph.png"; set grid; plot "/dev/stdin" w lp lw 4 title "Time taken"'
-
